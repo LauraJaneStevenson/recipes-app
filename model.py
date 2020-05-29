66ecf412cb55fb2b251f69a058a84f2ea6610824
LauraJaneStevenson <laurajanestevenson628@gmail.com>
2020-05-29 18:46:28 +0000

    fixed model.py

--- model.py
+++ model.py
@@ -39,10 +39,16 @@ class Recipe(db.Model):
                            primary_key=True)
     recipe_name = db.Column(db.String,
                             nullable=False)
+    description = db.Column(db.Text,
+                            nullable=True)
     user_id = db.Column(db.Integer,
                         db.ForeignKey('users.user_id'),
                         nullable=False)
     date = db.Column(db.DateTime,nullable=False)
+
+    instuctions = db.Column(db.Text,
+                            nullable=True)
+    recipe_ingredient = db.relationship('RecipeIngredient')
 
     user = db.relationship("User")
 
@@ -52,6 +58,46 @@ class Recipe(db.Model):
 
         return f"<Recipe_Name: {self.recipe_name} Recipe_ID: {self.recipe_id} User_ID: {self.user_id}>"
 
+class Ingredient(db.Model):
+    """Data model for ingredients"""
+    __tablename__ = "ingredients"
+
+    ingredient_id = db.Column(db.Integer,
+                           autoincrement=True, 
+                           primary_key=True)
+    ingredient_name = db.Column(db.String,
+                            nullable=False)
+    recipe_ingredient = db.relationship('RecipeIngredient')
+
+    def __repr__(self):
+        """Returns readable info about ingredient objects"""
+        return f"<Ingredient_Name: {self.ingredient_name}, Ingredient_ID: {self.ingredient_id}>"
+
+class RecipeIngredient(db.Model):
+    """Data model for an ingredient used in a recipe"""
+
+    __tablename__ = "recipe_ingredients"
+
+    RecipeIngredient_id = db.Column(db.Integer,
+                           autoincrement=True, 
+                           primary_key=True)
+    ingredient_id = db.Column(db.Integer,
+                        db.ForeignKey('ingredients.ingredient_id'),
+                        nullable=False)
+    recipe_id = db.Column(db.Integer,
+                        db.ForeignKey('recipes.recipe_id'),
+                        nullable=False)
+    quantity = db.Column(db.String,
+                     nullable=False)
+
+    recipe = db.relationship('Recipe')
+
+    ingredient = db.relationship('Ingredient')
+
+    def __repr__(self):
+        """Returns readable info about recipe_ingredient objects"""
+
+        return f"<RI_ID: {self.RecipeIngredient_id} Ingredient_ID: {self.ingredient_id}, Recipe: {self.recipe_id}"
 
 # Connect db to app
 def connect_to_db(app,dburi='postgres:///recipes'):
